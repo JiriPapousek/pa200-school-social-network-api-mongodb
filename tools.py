0ac314474c68b188d8626cf58deccf31ca953f1b
Jiří Papoušek <jipapous@redhat.com>
2023-05-19 15:16:26 +0200

    Change function to send failed auth notification

--- tools.py
+++ tools.py
@@ -8,15 +8,10 @@ SERVICE_BUS_CONN_STRING = os.getenv("CUSTOMCONNSTR_SERVICE_BUS")
 
 
 async def send_failed_auth_message(user: schemas.User):
-    try:
-        message = ServiceBusMessage(user.username)
-        async with ServiceBusClient.from_connection_string(
-                conn_str=SERVICE_BUS_CONN_STRING,
-                logging_enable=True) as servicebus_client:
-            sender = servicebus_client.get_queue_sender(queue_name="failed_auth")
-            async with sender:
-                await sender.send_messages(message)
-
-    except Exception as e:
-        print(e)
-        return str(e)
+    message = ServiceBusMessage(user.username)
+    async with ServiceBusClient.from_connection_string(
+            conn_str=SERVICE_BUS_CONN_STRING,
+            logging_enable=True) as servicebus_client:
+        sender = servicebus_client.get_queue_sender(queue_name="failed_auth")
+        async with sender:
+            await sender.send_messages(message)

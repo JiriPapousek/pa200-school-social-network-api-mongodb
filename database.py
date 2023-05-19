e7c22c756c0a3485cf8e66e9e2f440f70398a855
Jiří Papoušek <jipapous@redhat.com>
2023-05-19 13:47:53 +0200

    Change env name of mongo connection string

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import os
 import motor.motor_asyncio
 
-MONGODB_CONNECTION_STR = os.getenv("CUSTOMCONNSTR_MONGODB_CONNECTION_STR")
+MONGODB_CONNECTION_STR = os.getenv("CUSTOMCONNSTR_MONGODB")
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_CONNECTION_STR)
 db = client.pa200db
